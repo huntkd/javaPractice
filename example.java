766fc889dfbf450b2c3f035815e6d3b4a2492feb
Keelin Hunt <huntkd@s.dcsdk12.org>
2018-01-11 12:12:29 -0700

    example

---
+++ example.java
@@ -0,0 +1,31 @@
+// import java.util.Scanner;
+// import java.util.Random;
+
+// class Example {
+
+// 	public static void main(String[] args) {
+// 		// for (String str : args) {
+// 		// 	System.out.println(str);
+// 		// }
+
+// 		// int n = Integer.parseInt(args[0]);
+// 		// double a = Double.parseDouble(args[1]);
+// 		// System.out.println(a / 2.0);
+
+// 		// Scanner scanner = new Scanner(System.in);
+
+// 		// System.out.println("Type 2 numbers:");
+// 		// int x = scanner.nextInt();
+// 		// double y = scanner.nextDouble();
+// 		// System.out.println("You typed " + x);
+// 		// System.out.println("You typed " + y);
+
+// 		Random random = new Random();
+
+// 		for(int i=0; i<100; i++) {
+// 			int r = random.nextInt(10) + 1;
+// 			System.out.println(r);
+// 		}
+// 	}
+
+// }

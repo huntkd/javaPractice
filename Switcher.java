da6d06c5de3cbe5fbe33d4a7504b83885352f3b1
Keelin Hunt <huntkd@s.dcsdk12.org>
2018-01-16 11:19:24 -0700

    Started Switcher

---
+++ Switcher.java
@@ -0,0 +1,27 @@
+class Switcher {
+
+	public static void main(String[] args) {
+		
+	}
+
+	public static String switcher(String intial, String swapA, String swapB) {
+		for (int i=0; i.swapA.length() >= intial.length; i++) {
+			seg = intial.substring(i, i+swapA.length());
+			if (seg.eql(swapA)) {
+				// save the place for swap later
+			}
+		}
+		for (int i=0; i.swapB.length() >= intial.length; i++) {
+			seg = intial.substring(i, i+swapA.length());
+			if (seg.eql(swapB))  {
+				// save the place for swap later
+			}
+		}
+		for () {
+
+
+		}
+
+	}
+
+}

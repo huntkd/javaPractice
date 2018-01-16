29e8ed7bb5243e62fd7a816997a3bd5e40c0ad6d
Keelin Hunt <huntkd@s.dcsdk12.org>
2018-01-16 10:44:31 -0700

    Make second comand optional

--- Best.java
+++ Best.java
@@ -2,16 +2,20 @@ class Best {
 
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]);
-		int e = Integer.parseInt(args[1]);
+		String message = "YOU'RE THE BEST";
 
-		String str = "";
-
-		for (int i=0; i<e; i++) {
-			str = str + "!";
-		}
-
-		for (int i=0; i<n; i++) {
-			System.out.println("YOU'RE THE BEST" + str);
+		if (args.length > 1) {
+			int e = Integer.parseInt(args[1]);
+			for (int i=0; i<e; i++) {
+				message = message + "!";
+			}
+			for (int i=0; i<n; i++) {
+				System.out.println(message);
+			}
+		} else {
+			for (int i=0; i<n; i++) {
+				System.out.println(message);
+			}
 		}
 
 	}

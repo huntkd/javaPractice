eaf6cb784362d377ebc6505d661900c13b2b1c4f
Keelin Hunt <huntkd@s.dcsdk12.org>
2018-01-18 05:54:19 -0700

    Work on switcher for homework

--- Switcher.java
+++ Switcher.java
@@ -1,27 +1,29 @@
 class Switcher {
 
 	public static void main(String[] args) {
-		
+		switcher(args[0], args[1], args[3]);
 	}
 
-	public static String switcher(String intial, String swapA, String swapB) {
-		for (int i=0; i.swapA.length() >= intial.length; i++) {
-			seg = intial.substring(i, i+swapA.length());
-			if (seg.eql(swapA)) {
-				// save the place for swap later
+	public static String switcher(String initial, String swapA, String swapB) {
+		String temporary = new String("TEMPORARY");
+		for (int i=0; i+swapA.length() >= initial.length(); i++) {
+			String seg = initial.substring(i, i+swapA.length());
+			if (seg.equals(swapA)) {
+				String replaceString = seg.replace(seg, temporary);
 			}
 		}
-		for (int i=0; i.swapB.length() >= intial.length; i++) {
-			seg = intial.substring(i, i+swapA.length());
-			if (seg.eql(swapB))  {
-				// save the place for swap later
+		for (int i=0; i+swapB.length() >= initial.length(); i++) {
+			String seg = initial.substring(i, i+swapB.length());
+			if (seg.equals(swapB))  {
+				String replaceString = seg.replace(seg, swapA);
 			}
 		}
-		for () {
-
-
+		for (int i=0; i+temporary.length() >= initial.length(); i++) {
+			String seg = initial.substring(i, i+temporary.length());
+			if (seg.equals(temporary)) {
+				String replaceString = seg.replace(seg, swapB);
+			}
 		}
-
+		System.out.println(initial);
 	}
-
 }
